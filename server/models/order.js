978a47de46ebcfa6b5449c502f8e4cb814a933d3
Abrar Ahmad <abrarahmad28128@gmail.com>
2025-07-19 21:23:11 +0500

    add models code

---
+++ server/models/order.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+}, { _id: false });
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  orderItems: [orderItemSchema],
+  shippingAddress: {
+    type: String,
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
+  paidAt: {
+    type: Date,
+  },
+  isDelivered: {
+    type: Boolean,
+    default: false,
+  },
+  deliveredAt: {
+    type: Date,
+  },
+}, {
+  timestamps: true,
+});
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
